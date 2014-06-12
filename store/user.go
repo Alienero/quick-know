b61a78eb6bb5e3575feba03b892f4cb0f72c7908
Alienero <yiyan.lu@me.com>
2014-06-12 22:59:30 +0800

    fix user

--- store/user.go
+++ store/user.go
@@ -6,9 +6,9 @@ import (
 )
 
 type User struct {
-	id    string
-	psw   string
-	owner string // Owner
+	ID    string
+	Psw   string
+	Owner string // Owner
 }
 type Ctrl struct {
 	id   string
@@ -19,7 +19,7 @@ func Client_login(id, psw, owner string) bool {
 	sei := sei_user.New()
 	c := sei.DB(Config.UserName).C(Config.Clients)
 	var u *User
-	err := c.Find(bson.M{"id": id, "psw": psw, "owner": owner}).One(u)
+	err := c.Find(bson.M{"ID": id, "Psw": psw, "Owner": owner}).One(u)
 	if err != nil {
 		glog.Errorf("find user error:%v", err)
 		return false
