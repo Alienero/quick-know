e9442ba1978f378f04ef09a22e6b8272e9558ba9
Alienero <yiyan.lu@me.com>
2014-06-19 01:19:49 +0800

    fix the uuid ues nano

--- web/uuid.go
+++ web/uuid.go
@@ -19,7 +19,7 @@ var lock = new(sync.Mutex)
 func get_uuid() string {
 	h := md5.New()
 	lock.Lock()
-	io.WriteString(h, strconv.FormatInt(time.Now().Unix(), 10))
+	io.WriteString(h, strconv.FormatInt(time.Now().UTC().UnixNano(), 10))
 	lock.Unlock()
 	return strings.Replace(fmt.Sprintf("% x", h.Sum(nil)), " ", "", -1)
 }
