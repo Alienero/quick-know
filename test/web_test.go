35f6f61430d1970b3813983eb436a9b08e9f008b
Alienero <yiyan.lu@me.com>
2014-09-08 16:05:14 +0800

    add web API test

--- test/web_test.go
+++ test/web_test.go
@@ -11,41 +11,7 @@ import (
 	"github.com/Alienero/quick-know/store"
 )
 
-// func TestAddUser(t *testing.T) {
-// 	u := &store.User{Psw: "1024"}
-// 	data, err := json.Marshal(u)
-// 	if err != nil {
-// 		t.Error(err)
-// 		return
-// 	}
-
-// 	req, err := http.NewRequest("POST", "http://127.0.0.1:9901/push/add_user", bytes.NewReader(data))
-// 	if err != nil {
-// 		t.Error(err)
-// 		return
-// 	}
-// 	c := new(http.Client)
-// 	str := base64.StdEncoding.EncodeToString([]byte("test001"))
-// 	// req.SetBasicAuth("username", "password")
-
-// 	req.Header.Add("Authorization", " Basic "+str)
-// 	resp, err := c.Do(req)
-// 	if err != nil {
-// 		t.Error(err)
-// 		return
-// 	}
-// 	s, err := ioutil.ReadAll(resp.Body)
-// 	if err != nil {
-// 		println(err.Error())
-// 	}
-// 	println(string(s))
-
-// 	// Add private msg
-
-// 	// req,err = http.NewRequest("POST", "http://127.0.0.1:9901", body)
-// }
-
-func addMsg(t *testing.T) {
+func TestPrivateMsg(t *testing.T) {
 	u := &store.Msg{Body: []byte("这是离线消息2"), To_id: "29d2b76f47e4f2e36e732a53c74e2731"}
 	data, err := json.Marshal(u)
 	if err != nil {
@@ -73,8 +39,4 @@ func addMsg(t *testing.T) {
 		println(err.Error())
 	}
 	println(string(s))
-}
-
-func TestPrivateMsg(t *testing.T) {
-	addMsg(t)
 }
