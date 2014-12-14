1dc9f1f4607421ddf433c7dd8111ce2784fe6642
Alienero <yiyan.lu@me.com>
2014-12-14 13:44:08 +0800

    signal support windows and unix

---
+++ signal/signal_unix.go
@@ -0,0 +1,42 @@
+// Copyright © 2014 Alienero. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build darwin dragonfly freebsd linux netbsd openbsd
+
+package signal
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/golang/glog"
+)
+
+// InitSignal register signals handler.
+func InitSignal() chan os.Signal {
+	c := make(chan os.Signal, 1)
+	// signal.Notify(c, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT, syscall.SIGSTOP)
+	// In windows
+	signal.Notify(c, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT)
+	return c
+}
+
+// HandleSignal fetch signal from chan then do exit or reload.
+func HandleSignal(c chan os.Signal) {
+	// Block until a signal is received.
+	for {
+		s := <-c
+		glog.Infof("comet get a signal %s", s.String())
+		switch s {
+		case syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGSTOP, syscall.SIGINT:
+			return
+		case syscall.SIGHUP:
+			// TODO reload
+			//return
+		default:
+			return
+		}
+	}
+}
